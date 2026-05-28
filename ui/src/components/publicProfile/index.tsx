17a56fc58bc3b966793eccf8a3b853341b1412d8
Amir Hesamian <hesamian@uwm.edu>
2026-05-28 09:01:15 -0500

    fix: show spinner while loading public profile

--- ui/src/components/publicProfile/index.tsx
+++ ui/src/components/publicProfile/index.tsx
@@ -2,20 +2,28 @@ import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import {ExtendedProfileType} from "@geo-cast/lib/dto/account";
 import { getUserPublicProfile } from "../../actions";
+import { Spinner } from "../common";
 import _ from "lodash";
 
 const PublicProfile = () => {
   const { userId } = useParams();
 
   const [profile, setProfile] = useState<ExtendedProfileType>();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     if (userId) {
+      setLoading(true);
       getUserPublicProfile(userId)
         .then(x => setProfile(x.data))
-        .catch(() => setProfile(undefined));
+        .catch(() => setProfile(undefined))
+        .finally(() => setLoading(false));
     }
   }, [userId]);
+
+  if (loading) {
+    return <Spinner />;
+  }
 
   return (
     <>
